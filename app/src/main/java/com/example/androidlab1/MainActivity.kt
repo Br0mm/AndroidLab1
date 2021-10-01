ddcdab6975f4a6f2cb66358fdd7e5f83391b97aa
Br0mm <andrew-ost@mail.ru>
2021-10-02 00:51:54 +0300

    lab1 complete

--- app/src/main/java/com/example/androidlab1/MainActivity.kt
+++ app/src/main/java/com/example/androidlab1/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.androidlab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_1_14)
+        findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.rick_astley)
     }
 }
